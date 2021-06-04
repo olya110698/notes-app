955a956dd46282fb2337bff930ba1cfb20a11811
olya110698 <78224530+olya110698@users.noreply.github.com>
2021-06-04 19:34:44 +0300

    создали alert

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import React from 'react'
-import {BrowserRouter, Route, Switch} from 'react-router-dom'
-import {Home} from './pages/Home'
-import {About} from './pages/About'
-import {Navbar} from './components/Navbar'
-
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { About } from "./pages/About";
+import { Navbar } from "./components/Navbar";
+import { Alert } from "./components/Alert";
 
 function App() {
   return (
     <BrowserRouter>
-    <Navbar />
+      <Navbar />
       <div className="container pt-4">
+        <Alert />
         <Switch>
-           <Route path={'/'} exact component={Home} />
-           <Route path={'/about'} component={About} />
+          <Route path={"/"} exact component={Home} />
+          <Route path={"/about"} component={About} />
         </Switch>
       </div>
-         </BrowserRouter>
+    </BrowserRouter>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,33 +1,26 @@
-import React from 'react'
-import {NavLink} from 'react-router-dom'
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 export const Navbar = () => (
-    <nav className="navbar navbar-dark navbar-expand-lg bg-primary">
-        <div className="navbar-brand">
-            Notes App
-        </div>
+  <nav className="navbar navbar-dark navbar-expand-lg bg-primary">
+    <div className="navbar-brand">Notes App</div>
 
-        <ul className="navbar-nav">
-        <li className="nav-item">
-          <NavLink
-           className="nav-link"
-            to="/"
-            exact                   //для того, чтобы ссылка подсвечивалась активной
-            >
-                Главная
-          </NavLink>
-        </li>
-        
-        <li className="nav-item">
+    <ul className="navbar-nav">
+      <li className="nav-item">
         <NavLink
-           className="nav-link"
-            to="/about"
-            >
-                Информация
-          </NavLink> 
-        </li>
-        
-      </ul>
+          className="nav-link"
+          to="/"
+          exact //для того, чтобы ссылка подсвечивалась активной
+        >
+          Главная
+        </NavLink>
+      </li>
 
-    </nav>
-)
+      <li className="nav-item">
+        <NavLink className="nav-link" to="/about">
+          Информация
+        </NavLink>
+      </li>
+    </ul>
+  </nav>
+);
