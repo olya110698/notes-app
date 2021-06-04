9638d740f1604bf4ba77bbaddea90e6b37c76d4b
olya110698 <78224530+olya110698@users.noreply.github.com>
2021-06-04 19:00:13 +0300

    добавили дату и сместили ее

--- src/components/notes.js
+++ src/components/notes.js
@@ -1,24 +1,19 @@
-import React from 'react'
+import React from "react";
 
-export const Notes = ({notes }) => {
-    return (
-        <ul className="list-group">
-            {notes.map(note => (
-                <li 
-                className="list-group-item note"
-                key={note.id}
-                >
-                    {note.title}
-                    
-                    <button 
-                        type="button" 
-                        className="btn btn-outline-danger btn-sm"
-                        >
-                             &times;         {/* крестик*/}
-                    </button>   
-                </li>
-            ))}
-            
-        </ul>
-    )
-}
+export const Notes = ({ notes }) => {
+  return (
+    <ul className="list-group">
+      {notes.map((note) => (
+        <li className="list-group-item note" key={note.id}>
+          <div>
+            <strong>{note.title}</strong>
+            <small>{new Date().toLocaleDateString()}</small>
+          </div>
+          <button type="button" className="btn btn-outline-danger btn-sm">
+            &times; {/* крестик*/}
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+};
