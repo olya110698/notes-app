e56a3c9d2c16fa944c357e27da6e83d5fc732e27
olya110698 <78224530+olya110698@users.noreply.github.com>
2021-06-05 12:39:39 +0300

    сделали логику в бд

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,15 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useContext, useEffect } from "react";
 import { Form } from "../components/Form";
 import { Notes } from "../components/Notes";
+import { FirebaseContext } from "../context/firebase/firebaseContext";
 
 export const Home = () => {
-  const notes = new Array(3)
-    .fill("")
-    .map((_, i) => ({ id: i, title: `Note ${i + 1}` }));
+  const { loading, notes, fetchNotes } = useContext(FirebaseContext);
+
+  useEffect(() => {
+    fetchNotes();
+    // eslint-disable-next-line
+  }, []);
 
   return (
     <Fragment>
