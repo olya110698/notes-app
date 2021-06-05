c5c472c98c80db56c1151a6e7da26706f168138a
olya110698 <78224530+olya110698@users.noreply.github.com>
2021-06-05 15:28:23 +0300

    сделали удаление заметок

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,8 @@ import { Notes } from "../components/Notes";
 import { FirebaseContext } from "../context/firebase/firebaseContext";
 
 export const Home = () => {
-  const { loading, notes, fetchNotes } = useContext(FirebaseContext);
+  const { loading, notes, fetchNotes, removeNote } =
+    useContext(FirebaseContext);
 
   useEffect(() => {
     fetchNotes();
@@ -17,7 +18,7 @@ export const Home = () => {
       <Form />
 
       <hr />
-      {loading ? <Loader /> : <Notes notes={notes} />}
+      {loading ? <Loader /> : <Notes notes={notes} onRemove={removeNote} />}
     </Fragment>
   );
 };
