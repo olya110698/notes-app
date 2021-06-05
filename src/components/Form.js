02a34624fadb638c4dc539970a87f7df7bd1bf98
olya110698 <78224530+olya110698@users.noreply.github.com>
2021-06-05 11:16:15 +0300

    подключили базу данных

--- src/components/Form.js
+++ src/components/Form.js
@@ -8,7 +8,13 @@ export const Form = () => {
   const submitHandler = (event) => {
     event.preventDefault();
 
-    alert.show(value, "success");
+    if (value.trim()) {
+      //...
+      alert.show("Заметка была создана", "success");
+      setValue("");
+    } else {
+      alert.show("Введите название заметки");
+    }
   };
 
   return (
