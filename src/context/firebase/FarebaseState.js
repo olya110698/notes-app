420cd7b82a3af4bea643f35d68ebed87d3c2f0cb
olya110698 <78224530+olya110698@users.noreply.github.com>
2021-06-05 15:14:33 +0300

    прикрутили firebase

--- src/context/firebase/FarebaseState.js
+++ src/context/firebase/FarebaseState.js
@@ -2,9 +2,9 @@ import React, { useReducer } from "react";
 import axios from "axios";
 import { FirebaseContext } from "./firebaseContext";
 import { firebaseReducer } from "./firebaseReducer";
-import { REMOVE_NOTE, SHOW_LOADER } from "../types";
+import { ADD_NOTE, FETCH_NOTES, REMOVE_NOTE, SHOW_LOADER } from "../types";
 
-const url = process.env.REACT_APP_DB_UPL;
+const url = process.env.REACT_APP_DB_URL;
 
 export const FirebaseState = ({ children }) => {
   const initialState = {
@@ -17,8 +17,18 @@ export const FirebaseState = ({ children }) => {
 
   const fetchNotes = async () => {
     showLoader();
-    const res = await axios.get(`${url}/notes.json`);
-    console.log("fetchNotes", res.data);
+    const res = await axios.get(`${url}/notes.json`); //notes это ключ для всех note
+
+    const payload = Object.keys(res.data).map((key) => {
+      return {
+        ...res.data[key],
+        id: key,
+      };
+    });
+    dispatch({
+      type: FETCH_NOTES,
+      payload,
+    });
   };
 
   const addNote = async (title) => {
@@ -27,9 +37,20 @@ export const FirebaseState = ({ children }) => {
       date: new Date().toJSON(),
     };
 
-    const res = await axios.post(`${url}/notes.json`, note);
+    try {
+      const res = await axios.post(`${url}/notes.json`, note);
+      const payload = {
+        ...note,
+        id: res.data.name,
+      };
 
-    console.log("addNote", res.data);
+      dispatch({
+        type: ADD_NOTE,
+        payload,
+      });
+    } catch (e) {
+      throw new Error(e.message);
+    }
   };
 
   const removeNote = async (id) => {
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import React, { Fragment, useContext, useEffect } from "react";
 import { Form } from "../components/Form";
+import { Loader } from "../components/Loader";
 import { Notes } from "../components/Notes";
 import { FirebaseContext } from "../context/firebase/firebaseContext";
 
@@ -16,8 +17,7 @@ export const Home = () => {
       <Form />
 
       <hr />
-
-      <Notes notes={notes} />
+      {loading ? <Loader /> : <Notes notes={notes} />}
     </Fragment>
   );
 };
