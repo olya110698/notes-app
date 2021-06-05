1fe83398000fb7efbb66b44564a56553f2eeb3be
olya110698 <78224530+olya110698@users.noreply.github.com>
2021-06-05 10:58:22 +0300

    создали связь alert

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,14 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
+import { AlertContext } from "../context/alert/alertContext";
 
-export const Alert = ({ alert }) => {
-  if (!alert) {
+export const Alert = () => {
+  const { alert, hide } = useContext(AlertContext);
+  if (!alert.visible) {
     return null;
   }
   return (
     <div className={`alert alert-${alert.type || "warning"} alert-dismissible`}>
       <strong>Внимание!</strong>
       {alert.text}
-      <button type="button" className="close" aria-label="Close">
+      <button onClick={hide} type="button" className="close" aria-label="Close">
         <span aria-hidden="true">&times;</span>
       </button>
     </div>
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,16 +1,27 @@
-import React from 'react'
+import React, { useState, useContext } from "react";
+import { AlertContext } from "../context/alert/alertContext";
 
 export const Form = () => {
-    return (
-        <form>
-            <div className="form-group">
-                <input 
-                    type="text"
-                    className="form-control"
-                    placeholder="Введите название заметки"
-                />
+  const [value, setValue] = useState("");
+  const alert = useContext(AlertContext);
 
-            </div>
-        </form>
-    )
-}
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    alert.show(value, "success");
+  };
+
+  return (
+    <form onSubmit={submitHandler}>
+      <div className="form-group">
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Введите название заметки"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+      </div>
+    </form>
+  );
+};
